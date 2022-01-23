681437c92c92d2a4b8fa075a2205213708b59b10
Wrldyazzee <98255041+Wrldyazzee@users.noreply.github.com>
2022-01-23 10:44:55 +0100

    Revert "Update and rename classic.py to https:/Python (#1)"
    This reverts commit 893470b5958eaa8066465128993971af4766c8ff.

---
+++ classic.py
@@ -0,0 +1,12 @@
+pkg update && pkg upgrade
+$ pkg install python
+$ pkg install python3
+$ pkg install git
+S pip install requests
+$ pip install mechanize
+$ pip install lolcat
+$ pip install bs4
+$ rm-rf classic
+$ git clone
+S cd classic
+S python classic.py
